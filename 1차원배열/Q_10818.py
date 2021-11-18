3ada08c7036bf3fe4a413b7a83045a7fa5cbe124
asdfmelody <heesunmelody@gmail.com>
2021-11-18 18:45:52 +0900

    Q_2562 success

---
+++ 1차원배열/Q_10818.py
@@ -0,0 +1,4 @@
+#최소, 최대
+N=int(input())
+arr=list(map(int,input().split()))
+print(min(arr), max(arr))
---
+++ 1차원배열/Q_2562.py
@@ -0,0 +1,7 @@
+#최댓값
+arr=[]
+for a in range(9):
+    arr.append(int(input()))
+
+print(max(arr))
+print(arr.index(max(arr))+1)
