9c0ad3703e5b7b04e1549339b3eda7a175a71326
asdfmelody <heesunmelody@gmail.com>
2021-11-24 16:05:01 +0900

    Fix Q_4673

--- 함수/Q_4673.py
+++ 함수/Q_4673.py
@@ -1,13 +1,13 @@
-def self_number(a):
+def d(a):
     result=a
     intlist=list(map(int, str(a))) #정수의 각자리수를 list 형태로 저장
     result+=sum(intlist)
     return result
 
-#boolean list의 index값 이용
+#boolean list의 index값 이용 1~10000
 arr=[False for i in range (10001)] #길이가 정해진 리스트 만들기
 for a in range(10000):
-    if self_number(a) <=10000:
-        arr[self_number(a)]=True
+    if d(a) <=10000:
+        arr[d(a)]=True
 for b in range(10000):
     if arr[b] != True : print(b)
