8b0371f9bea7d5e832eafeec149e969af7f46cd7
asdfmelody <heesunmelody@gmail.com>
2021-11-29 21:33:43 +0900

    Q_2839 success

--- 스터디_1주차/Q_2839.py
+++ 스터디_1주차/Q_2839.py
@@ -1,1 +1,13 @@
 # 설탕 배달
+sugar=int(input())
+five=int(sugar/5)
+number=[]
+for i in range(five+1): #0~3
+    if i==0 and sugar%3==0:
+        number.append(int(sugar/3))
+    if i>0 and (sugar-i*5)%3 == 0 :
+        three=int((sugar-i*5)/3)
+        number.append(i + three)
+
+if len(number) >0 : print(min(number))
+else : print(-1)
