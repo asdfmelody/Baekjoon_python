7393bc3bfe1a29748f39c1577301cce605169239
asdfmelody <heesunmelody@gmail.com>
2021-11-29 20:54:20 +0900

    Q_1316 success

--- 문자열/Q_1316.py
+++ 문자열/Q_1316.py
@@ -1,8 +1,12 @@
-#그룹 단어 체커
-word=input()
+# 그룹 단어 체커
+# 다시 풀어보기
+group_count=n=int(input())
 
-for i in range(len(word)):
-    if word[i] == word[i+1]:
-        word=word[:i]+word[i]+word[i+2:]
-
-print(word)
+for i in range(n):
+    word=input()
+    for j in range(len(word)-1):
+        if word[j] != word[j+1]: # 다를때
+            if word[j+1] in word[:j]: # 지금 알파벳과 이전까지의 알파벳 비교
+                group_count-=1
+                break
+print(group_count)
