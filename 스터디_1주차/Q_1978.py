ba5544caef4ab891d63d47299b8350c229309c13
asdfmelody <heesunmelody@gmail.com>
2021-12-01 17:53:11 +0900

    Q_1978 success

---
+++ 스터디_1주차/Q_1978.py
@@ -0,0 +1,14 @@
+#소수 찾기
+
+a=int(input())
+arr=list(map(int, input().split()))
+result=len(arr)
+for i in range(len(arr)):
+    if arr[i] == 1 : result-=1
+    elif arr[i] > 1 :
+        for j in range(2,arr[i]):
+            if arr[i]%j == 0:
+                result-=1
+                break
+
+print(result)
