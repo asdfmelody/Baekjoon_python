ce6c0ad32421da0b9fc6bfafe31894cd422dda39
asdfmelody <heesunmelody@gmail.com>
2021-12-02 14:19:46 +0900

    Q_10250 success

---
+++ 스터디_1주차/Q_10250.py
@@ -0,0 +1,15 @@
+#ACM 호텔
+i=int(input())
+result=[]
+for a in range(i):
+    h,w,n=map(int,input().split())
+    if (n%h == 0) :
+        floor=h
+        ho=int(n/h)
+    else:
+        floor = n%h
+        ho=int(n/h)+1
+    result.append(ho+(floor*100))
+
+for a in range(i):
+    print(result[a])
