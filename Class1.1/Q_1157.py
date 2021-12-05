d618797fbe9fb40c01473ac722b6c9a36ed75e9f
asdfmelody <heesunmelody@gmail.com>
2021-12-05 22:52:17 +0900

    Q_1157 success

--- Class1.1/Q_1157.py
+++ Class1.1/Q_1157.py
@@ -1,1 +1,17 @@
-#
+# 단어 공부
+# A 65     Z 90
+word=input().upper()
+count=[0] *26
+
+for i in range(len(word)):
+    count[ord(word[i])-65]+=1
+
+a=0
+
+for i in range(26):
+    if max(count) == count[i] :
+        a+=1
+        result=chr(i+65)
+
+if a==1: print(result)
+else : print('?')
