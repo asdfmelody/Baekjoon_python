6fe34c8a3f32b70c02f2572f0e356ba30778eae5
asdfmelody <heesunmelody@gmail.com>
2021-12-10 21:40:14 +0900

    Q_1620 success

--- 스터디_3주차/Q_1620.py
+++ 스터디_3주차/Q_1620.py
@@ -1,11 +1,20 @@
 # 나는야 포켓몬 마스터 이다솜
+
+import sys
+input=sys.stdin.readline # input() -> sys.stdin.readline()
+
 N,M=map(int,input().split())
-dogam=[]
-Q=[]
-for _ in range(N):
-    dogam.append(input())
+
+dogam={} #dictionary 이용하면 list append 보다 빠름
+
+for i in range(1,N+1):
+    name=input().rstrip() # 문자열 입력받을때 .rstrip()
+    dogam[i]=name
+    dogam[name]=i
 
 for _ in range(M):
-    Q.append(input())
-
-print(dogam,Q)
+    Q=input().rstrip()
+    if Q.isdigit() :
+        print(dogam[int(Q)])
+    else:
+        print(dogam[Q])
