ffe90d7a588ed370b70d008204bdb85bc2f487d7
asdfmelody <heesunmelody@gmail.com>
2021-12-12 17:23:58 +0900

    Q_10799 success

--- study3/Q_10799.py
+++ study3/Q_10799.py
@@ -1,19 +1,16 @@
 # 쇠막대기
 bar=input()
+bar=bar.replace("()","0")
 result=0
 count = 0 # 겹쳐있는 막대기 개수 (층)
 
-i=0
-while i<len(bar):
-    if bar[i] == "(":
+for i in bar:
+    if i == "(":
         count+=1
-        if bar[i+1] ==")": # lazer
-            count-=1
-            result+=count
-            i+=1
-    elif bar[i] ==")" and count>0:
+    elif i=="0":
+        result+=count
+    else:
         count-=1
         result+=1
-    i += 1
 
 print(result)
