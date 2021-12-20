f17c4da07423541b0fb78db28a30c963fa363f9f
asdfmelody <heesunmelody@gmail.com>
2021-12-20 23:54:08 +0900

    2309 on progress

---
+++ study4/2309.py
@@ -0,0 +1,21 @@
+# 일곱 난쟁이
+# 3kg 5kg 문제랑 비슷한 느낌
+fake_nanjang=[]
+for _ in range(9):
+    fake_nanjang.append(int(input()))
+
+heightsum=sum(fake_nanjang)
+
+for i in range(9):
+    for j in range(i+1,9):
+        mustbe100=0
+        mustbe100=heightsum-fake_nanjang[i]-fake_nanjang[j]
+        if mustbe100 == 100:
+            del fake_nanjang[i]
+            del fake_nanjang[j]
+            print("found it")
+            break
+    if mustbe100 == 100: break
+
+for i in sorted(fake_nanjang):
+    print(i)
