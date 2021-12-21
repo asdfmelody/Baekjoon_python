4507f88039baf3d78ca8bb224843675de0abc1fd
asdfmelody <heesunmelody@gmail.com>
2021-12-21 14:07:38 +0900

    2864 success

--- study4/2864.py
+++ study4/2864.py
@@ -1,10 +1,20 @@
+# 5와 6의 차이
 A,B=input().split()
-A_pos = A.find('5'or'6')
-if A_pos != -1:
-    print("no")
-else:
-    A
+minA, maxA, minB, maxB="", "","", ""
+for i in range(len(A)):
+    if int(A[i])== 5 or int(A[i])== 6 :
+        minA+="5"
+        maxA+="6"
+    else:
+        minA+=A[i]
+        maxA+=A[i]
+for i in range(len(B)):
+    if int(B[i])== 5 or int(B[i])== 6 :
+        minB+="5"
+        maxB+="6"
+    else:
+        minB+=B[i]
+        maxB+=B[i]
 
 
-if '5' or '6' in B:
-    print("B yes")
+print(int(minA)+int(minB) , int(maxA)+int(maxB))
