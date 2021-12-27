3720ea4c1d94bdb27edd31355a90786b3a7ecd65
asdfmelody <heesunmelody@gmail.com>
2021-12-27 20:41:56 +0900

    2178 suceess

--- study5/2583.py
+++ study5/2583.py
@@ -1,4 +1,24 @@
 # 영역구하기
+from collections import deque
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def count(x,y):
+    queue=deque()
+
+    while queue: # queue가 빌때 까지 반복
+        x,y=queue.popleft()
+        for i in range(4): # 4방향으로 위치 확인
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx < 0 or nx>=N or ny<0 or ny>=M:
+                continue
+            if monun[nx][ny] >0:
+                continue
+            if monun[nx][ny] ==0:
+                monun[nx][ny]=2 # 방문한건 2로!
+                queue.append((nx,ny))
+    return monun
+
 M,N,K=map(int,input().split())
 monun=[[0 for col in range(N)] for row in range(M)] # [[0]*N]*M 하면 shallow copy,,
 
@@ -8,4 +28,4 @@ for _ in range(K):
         for y in range(M-y2, M-y1):
             monun[y][x]=1
 
-print(monun)
+print(count(0,0))
--- 그래프탐색/2178.py
+++ 그래프탐색/2178.py
@@ -10,13 +10,20 @@ def bfs(x,y):
         for i in range(4): # 4방향으로 위치 확인
             nx=x+dx[i]
             ny=y+dy[i]
-            if nx
+            if nx < 0 or nx>=n or ny<0 or ny>=m:
+                continue
+            if graph[nx][ny] ==0:
+                continue
+            if graph[nx][ny] ==1:
+                graph[nx][ny]=graph[x][y]+1
+                queue.append((nx,ny))
+    return graph[n-1][m-1]
 
-N,M=map(int, input().split())
+n,m=map(int, input().split())
 
 graph=[]
 
-for _ in range(N):
+for _ in range(n):
     graph.append(list(map(int,input())))
 
 dx=[-1,1,0,0]
